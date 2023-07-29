b3b375fe93ce75562a8855ac81e14b3f59188269
Firdavsbek07 <firdavsbekkomilov@gmail.com>
2023-07-29 21:55:37 +0800

    updated all paths

--- app.py
+++ app.py
@@ -7,23 +7,27 @@ app = Flask(__name__)
 def index():
     return render_template('html/index.html')
 
-@app.route("/products")
+@app.route("/products.html")
 def products():
     return render_template('html/products.html')
 
-@app.route("/product-details")
+@app.route("/product-details.html")
 def details():
     # Get the id query param from the request
     product_id = request.args.get('id')
     return render_template('html/product-details.html', product_id=product_id)
 
-@app.route("/contact")
+@app.route("/contact.html")
 def contacts():
-    return render_template('html/contact copy.html')
+    return render_template('html/contact.html')
 
-@app.route('/cart')
+@app.route('/cart.html')
 def cart():
     return render_template('html/cart.html')
+
+@app.route('/blog-single-sidebar.html')
+def blog():
+    return render_template('html/blog-single-sidebar.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
